89a03f8ba849ef7cb0461e45c672f49f51ffc187
wayne <waiting0819@msn.com>
2023-07-25 10:26:33 +0800

    add

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className='w-[640px] h-[360px]'>
-          <video ref={videoRef} id="test" src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
+          <video muted autoPlay ref={videoRef} id="test" src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4" />
         </div>
       </div>
     </main>
