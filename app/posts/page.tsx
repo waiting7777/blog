c2124f5d2b1f31ff53504a04490e94be0dd1aba1
waiting7777 <waiting0819@msn.com>
2023-08-11 06:35:29 +0800

    db test

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,12 @@
-function BlogPage() {
+import { sql } from "@vercel/postgres";
+
+async function BlogPage() {
+  const { rows } = await sql`SELECT * FROM test`
+  console.log(rows)
   return (
-    <div>123</div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">{rows.map(v => (
+      <div key={v.id}>{v.title}</div>
+    ))}</div>
   )
 }
 
