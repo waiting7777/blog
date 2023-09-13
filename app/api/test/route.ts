07e25afeb533c00f72b632ecd8dc34cc2b5e44d3
Waiting <waiting0819@msn.com>
2023-09-13 11:12:33 +0800

    ttt

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,5 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   console.log("Hello, world!")
+  console.log("test")
   return NextResponse.json({ message: "Hello, world!"});
 }
