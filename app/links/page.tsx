91dcb4957c3bede38240afe288d0005d2526fe6e
Waiting <waiting0819@msn.com>
2023-09-13 23:03:38 +0800

    add links

---
+++ app/links/page.tsx
@@ -0,0 +1,11 @@
+
+
+async function Links() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      links
+    </div>
+  )
+}
+
+export default Links
