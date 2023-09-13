a7f7af7969d194e320eaa7c762a6dc809546ffe9
Waiting <waiting0819@msn.com>
2023-09-13 09:54:38 +0800

    add sentry

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,10 @@
-import { sql } from "@vercel/postgres";
+
 
 async function BlogPage() {
-  const { rows } = await sql`SELECT * FROM test`
-  console.log(rows)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">{rows.map(v => (
-      <div key={v.id}>{v.title}</div>
-    ))}</div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      456
+    </div>
   )
 }
 
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0cecd5e2fe7adfae79c73056466c12cb@o413621.ingest.sentry.io/4505870602928129",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
